9a95b6f2f142a605059fd2b9e5e6618ae23ba2f1
Hana Hourston <hana.hourston@gmail.com>
2021-08-18 21:54:09 -0700

    20210818 backup

--- depth_range_gradient_checks.py
+++ depth_range_gradient_checks.py
@@ -1,6 +1,5 @@
 # To perform on value vs depth data
 
-
 import pandas as pd
 import numpy as np
 from tqdm import trange
@@ -79,11 +78,17 @@ def vvd_range_check(vvd, range_df):
 
 
 def vvd_gradient_check(df, grad_df):
+    # Value vs depth gradient check
+    # Check for gradients, inversions and zero sensitivity
+    # df: value vs depth dataframe
+    # grad_df: dataframe from WOA18 containing maximum gradient, inversion,
+    #          and zero sensitivity index values to check vvd data against
+
     df['Gradient_check_flag'] = np.zeros(len(df), dtype=int)
 
     prof_start_ind = np.unique(df.Profile_number, return_index=True)[1]
 
-    for i in trange(len(prof_start_ind)):  # len(prof_start_ind) 20
+    for i in trange(len(prof_start_ind)):  #  20
         # Set profile end index
         if i == len(prof_start_ind) - 1:
             end_ind = len(df)
@@ -112,14 +117,13 @@ def vvd_gradient_check(df, grad_df):
             d_gradients = np.diff(gradients)
 
             # Create flags accordingly
-            # What to do about Z = 400 m??
-            # If depth < 400m and gradient < -max, apply one set of criteria
-            # If depth > 400m and gradient < -max, apply other set of criteria
-            subsetter_MGV_lt_400 = np.where((depths < 400) &
+            # If depth <= 400m and gradient < -max, apply one set of criteria
+            # If depth > 400m and gradient < -max, apply other set of criteria...
+            subsetter_MGV_lt_400 = np.where((depths <= 400) &
                                             (gradients < -grad_df.loc['Oxygen', 'MGV_Z_lt_400m']))[0]
             subsetter_MGV_gt_400 = np.where((depths > 400) &
                                             (gradients < -grad_df.loc['Oxygen', 'MGV_Z_gt_400m']))[0]
-            subsetter_MIV_lt_400 = np.where((depths < 400) &
+            subsetter_MIV_lt_400 = np.where((depths <= 400) &
                                             (gradients > grad_df.loc['Oxygen', 'MIV_Z_lt_400m']))[0]
             subsetter_MIV_gt_400 = np.where((depths > 400) &
                                             (gradients > grad_df.loc['Oxygen', 'MIV_Z_gt_400m']))[0]
@@ -129,7 +133,7 @@ def vvd_gradient_check(df, grad_df):
             # If there are zero-as-missing-values at the very surface, then
             # the ZSI check wouldn't find them because it needs the gradient
             subsetter_ZSI_lt_400 = np.where(
-                (depths[1:] < 400) &
+                (depths[1:] <= 400) &
                 (d_gradients < -grad_df.loc[
                     'Oxygen', 'MGV_Z_lt_400m'] * grad_df.loc['Oxygen', 'ZSI']) &
                 (values[1:] == 0.))[0]
@@ -151,6 +155,18 @@ def vvd_gradient_check(df, grad_df):
     return df
 
 
+def vvd_apply_value_flag(df, flag_name):
+    # Apply flag and generate new copy of df
+    # Flag=0 means data passed the check so want to keep that
+
+    df = df.loc[df[flag_name] == 0]
+
+    df_return = df.drop(columns=flag_name)
+
+    return df_return
+
+
+##### STEP 1: Depth check
 nan_dir = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
           'value_vs_depth\\5_filtered_for_nans\\'
 
@@ -170,11 +186,20 @@ df_outdir = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
 
 df_out.to_csv(df_outdir + df_outname, index=False)
 
-# Export the updated dataframe
-# When to apply these flags?
+# Apply the flags in a separate df
+df_in_name = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
+             'value_vs_depth\\6_depth_check\\' \
+             'ALL_Oxy_1991_2020_value_vs_depth_dep_check.csv'
+
+df_in = pd.read_csv(df_in_name)
+
+df_out = vvd_apply_value_flag(df_in, 'Depth_check_flag')
+
+df_out_name = df_in_name.replace('dep_check', 'dep_check_done')
+df_out.to_csv(df_out_name, index=False)
 
 
-##### STEP 2
+##### STEP 2: Range check
 # Now do range checks: flag=0 if check passed; flag=1 if check failed
 # Use preset ranges from WOD
 range_file = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\literature\\' \
@@ -190,34 +215,44 @@ df_range = pd.read_csv(range_file)
 df_dir = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
          'value_vs_depth\\6_depth_check\\'
 
-df_file = 'ALL_Oxy_1991_2020_value_vs_depth_dep_check.csv'
+df_file = 'ALL_Oxy_1991_2020_value_vs_depth_dep_check_done.csv'
 
-df = pd.read_csv(df_dir + df_file)
+df_in = pd.read_csv(df_dir + df_file)
 
 # Range check function
-df_out = vvd_range_check(df, df_range)
+df_out = vvd_range_check(df_in, df_range)
 
 print(len(df_out.loc[df_out.Range_check_flag == 1, 'Range_check_flag']))
 indices_rng = np.where(df_out.Range_check_flag == 1)[0]
 print(df_out.loc[indices_rng, ['Depth_m', 'Value']])
-# 24176 rows that were flagged during this check: should limits be expanded??
+# 24174 rows that were flagged during this check: should limits be expanded??
 
-# Export the updated dataframe
-# When to apply these flags?
+# df_out.drop(columns='Depth_check_flag', inplace=True)
 
-df_outname = df_file.replace('dep_check', 'rng_check')
+df_outname = df_file.replace('dep_check_done', 'rng_check')
 df_outdir = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
             'value_vs_depth\\7_range_check\\'
-df.to_csv(df_outdir + df_outname, index=False)
+df_out.to_csv(df_outdir + df_outname, index=False)
+
+df_rng_name = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
+              'value_vs_depth\\7_range_check\\' \
+              'ALL_Oxy_1991_2020_value_vs_depth_rng_check.csv'
+
+df_in_fm = pd.read_csv(df_rng_name)
+
+df_out_fm = vvd_apply_value_flag(df_in_fm, 'Range_check_flag')
+
+df_out_name_fm = df_rng_name.replace('rng_check', 'rng_check_done')
+df_out_fm.to_csv(df_out_name_fm, index=False)
 
 
-### STEP 3
+##### STEP 3: Gradient checks
 # Now do gradient checks: flag=1 if check failed; flag=0 if check passed
 
 df_dir = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
          'value_vs_depth\\7_range_check\\'
 
-df_file = 'ALL_Oxy_1991_2020_value_vs_depth_rng_check.csv'
+df_file = 'ALL_Oxy_1991_2020_value_vs_depth_rng_check_done.csv'
 
 df_in = pd.read_csv(df_dir + df_file)
 
@@ -226,23 +261,23 @@ grad_file = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\literature\\' \
             'WOA docs\\wod18_users_manual_tables\\wod18_max_gradient_inversion.csv'
 df_grad = pd.read_csv(grad_file, index_col='Variable')
 
-# 0: good; 1: excessive gradient (negative gradient over depth);
-# 2: excessive inversion (positive gradient over depth)
-# 3: Zero sensitivity check failed ????????
-
+# Run gradient check
 df_out = vvd_gradient_check(df_in, df_grad)
 
+# Print summary statistics
 print(len(df_out.loc[df_out.Gradient_check_flag == 1, 'Gradient_check_flag']))  # gradient
 print(len(df_out.loc[df_out.Gradient_check_flag == 2, 'Gradient_check_flag']))  # inversion
 print(len(df_out.loc[df_out.Gradient_check_flag == 3, 'Gradient_check_flag']))  # ZSI
 print(len(df_out.loc[df_out.Gradient_check_flag == 4, 'Gradient_check_flag']))  # ZSI and gradient
 print(len(df_out.loc[df_out.Gradient_check_flag == 5, 'Gradient_check_flag']))  # ZSI and inversion
 
-df_outname = df_file.replace('rng_check', 'grad_check')
+df_outname = df_file.replace('rng_check_done', 'grad_check')
 df_outdir = 'C:\\Users\\HourstonH\\Documents\\NEP_climatology\\data\\' \
             'value_vs_depth\\8_gradient_check\\'
 
 df_out.to_csv(df_outdir + df_outname, index=False)
 
-# Export the updated dataframe
-# When to apply these flags?
+df_out2 = vvd_apply_value_flag(df_out, 'Gradient_check_flag')
+
+df_out2_name = df_outname.replace('grad_check', 'grad_check_done')
+df_out2.to_csv(df_out2_name, index=False)
